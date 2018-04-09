ce3e1e941d9f00a743aecc3008c0e7a01db1009f
Michaut.Francois <francois.michaut@epitech.eu>
2018-04-09 23:56:20 +0200

    Added is_foud() function Updated getBreadcrumb() function to return an array of the path followed to found the currentPage Checked all required tests

--- src/Breadcrumb.php
+++ src/Breadcrumb.php
@@ -1,6 +1,23 @@
 <?php
 declare(strict_types=1);
 
+function is_found($current, $target)
+{
+	$i = 0;
+
+	if ($current['name'] == $target)
+		return 1;
+	while (isset($current['child'][$i])) {
+		if ($current['child'][$i]['name'] == $target) {
+			return 1;
+		} elseif (is_found($current['child'][$i], $target) == 1) {
+			return 1;
+		}
+		$i = $i + 1;
+	}
+	return 0;
+}
+
 class Breadcrumb
 {
     private $menu;
@@ -9,9 +26,26 @@ class Breadcrumb
     {
         $this->menu = $menu;
     }
-
+	
     public function getBreadcrumb($currentPage)
     {
-        return [];
+	$res = array();
+	$i = 0;
+	$temp = $this->menu;
+
+	while (isset($temp[$i])) {
+		if ($temp[$i]['name'] == $currentPage) {
+			$res[] = $temp[$i]['name'];
+			break;
+		} elseif (is_found($temp[$i], $currentPage) == 1) {
+			$res[] = $temp[$i]['name'];
+			$temp = $temp[$i]['child'];
+			$i = -1;
+		}
+		$i = $i + 1;
+	}
+    return $res;
     }
+	
 }
+?>
